df1660b064926e8296e60174cd3ce9c5be73f863
Dmitry Levchenko <wirwl@hotmail.com>
2024-07-12 01:48:31 +0300

    Замена протухшего токена

--- src/entities/common/lib/apollo.ts
+++ src/entities/common/lib/apollo.ts
@@ -5,7 +5,7 @@ const httpLink = createHttpLink({
     uri: 'https://api.github.com/graphql',
 });
 
-export function initApolloClient(token: string="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") {
+export function initApolloClient(token: string="github_pat_"+"11AAEQXZY0FPV3Wnc9uy1t_fh97PeUJHGRfdNAtEdMeg5XPI0GFUECVBJOprZw8WJ1TYKDCTZGW9lzx4a3") {
     const authLink = setContext((_, { headers }) => {
         return {
             headers: {
