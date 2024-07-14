4f90d8e1c0b0883774d6349f13e77bf7b3eb5f3b
Dmitry Levchenko <wirwl@hotmail.com>
2024-07-14 23:37:52 +0300

    Добавлен playwright для e2e тестов и написал два теста

---
+++ src/pages/repos/ui/Index/index.test.ts
@@ -0,0 +1,19 @@
+import { test, expect } from '@playwright/test';
+
+test('Проверка заголовка у страницы', async ({ page }) => {
+  await page.goto('http://localhost:5173');
+  await expect(page).toHaveTitle("Tool-kit.tech тестовое задание");
+});
+
+test('При вводе очередного значения в инпут, должен сбрасываться номер страницы до первой', async ({ page }) => {
+  await page.goto('http://localhost:5173');
+
+  await page.getByRole('textbox').fill('eslint');
+  await page.locator('[class*=Paginator__root] > li').waitFor;
+  await page.locator('[class*=Paginator__root] > li').nth(2).click();
+
+  await page.getByRole('textbox').fill('babel');
+  await page.locator('[class*=Paginator__root] > li').waitFor;
+  const active = await page.locator('[class*=Paginator__root] [class*=Paginator__active]');
+  await expect(active).toHaveText('1');
+});
